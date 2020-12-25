9213d443fff9aa3aae684893b264a044a68f5bc3
Ben Weedon <ben@weedon.email>
2020-12-25 13:56:47 -0800

    Append text entry to page when alt+shift+m is pressed

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,1 +1,12 @@
-console.log('hi');
+document.addEventListener('keydown', event => {
+    if (event.altKey && event.shiftKey && event.key == 'M') {
+        const element = createInputElement();
+        document.body.appendChild(element);
+    }
+});
+
+function createInputElement() : HTMLInputElement {
+    const element = document.createElement('input');
+    element.id = 'macroll-input';
+    return element;
+}
