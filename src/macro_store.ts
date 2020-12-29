bfba6579494bf963eff8c5e03323741d2e6af25c
Ben Weedon <ben@weedon.email>
2020-12-28 19:06:03 -0800

    Add string modules to the page
    This requires setting the script src as a blob, since setting the source
    inline seems to break content security policies.

--- src/macro_store.ts
+++ src/macro_store.ts
@@ -12,9 +12,27 @@ export function initialize(): void {
             macros.dnd[name] = (dnd as Record<string, MacroFunction>)[name]!;
         }
     }
+
+    addModuleToPage('testmod', 'console.log("hey, this works!");');
 }
 
 export function get(name: string): MacroFunction {
     const [mod, macro] = name.split('.', 2);
     return macros[mod!]![macro!]!;
+}
+
+function addModuleToPage(name: string, content: string): void {
+    const id = `macroll-module-${name}`;
+    const existingScript = document.getElementById(id) as HTMLScriptElement | null;
+    if (existingScript !== null) {
+        URL.revokeObjectURL(existingScript.src);
+        existingScript.remove();
+    }
+
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.id = id;
+    // We need to use blob storage to get around content security policies.
+    script.src = URL.createObjectURL(new Blob([content], {type: 'application/javascript'}));
+    document.head.appendChild(script);
 }
