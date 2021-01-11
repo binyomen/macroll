f19a0f0d37003f5ed87a1383376b8f9340137d91
Ben Weedon <ben@weedon.email>
2021-01-11 14:05:04 -0800

    Add dnd.chk macro

--- static/builtins/dnd.js
+++ static/builtins/dnd.js
@@ -1,33 +1,54 @@
-macroll.registerMacro('atk',
-    async (name, atk, dmg, advantage = 'norm', charname = '') => {
-        const fields = {
-            attack: 1,
-            damage: 1,
-            rname: name,
-            r1: atk,
-            r2: atk,
-            mod: getModifier(atk),
-            dmg1: dmg,
-            dmg1flag: 1,
-            crit: 1,
-            crit1: dmg,
-        };
+macroll.registerMacro('atk', async (name, atk, dmg, advantage = 'norm', charname = '') => {
+    const fields = {
+        attack: 1,
+        damage: 1,
+        rname: name,
+        r1: atk,
+        r2: atk,
+        mod: getModifier(atk),
+        dmg1: dmg,
+        dmg1flag: 1,
+        crit: 1,
+        crit1: dmg,
+    };
 
-        if (advantage === 'adv') {
-            fields.advantage = 1;
-        } else if (advantage === 'dadv') {
-            fields.disadvantage = 1;
-        } else {
-            fields.normal = 1;
-        }
-
-        if (charname.length > 0) {
-            fields.charname = charname;
-        }
-
-        return await macroll.sendCommand({name: 'atkdmg', fields});
+    if (advantage === 'adv') {
+        fields.advantage = 1;
+    } else if (advantage === 'dadv') {
+        fields.disadvantage = 1;
+    } else {
+        fields.normal = 1;
     }
-);
+
+    if (charname.length > 0) {
+        fields.charname = charname;
+    }
+
+    return await macroll.sendCommand({name: 'atkdmg', fields});
+});
+
+macroll.registerMacro('chk', async (name, roll, advantage = 'norm', charname = '') => {
+    const fields = {
+        rname: name,
+        r1: roll,
+        r2: roll,
+        mod: getModifier(roll),
+    };
+
+    if (advantage === 'adv') {
+        fields.advantage = 1;
+    } else if (advantage === 'dadv') {
+        fields.disadvantage = 1;
+    } else {
+        fields.normal = 1;
+    }
+
+    if (charname.length > 0) {
+        fields.charname = charname;
+    }
+
+    return await macroll.sendCommand({name: 'simple', fields});
+});
 
 function getModifier(roll) {
     const inner = roll.inner;
