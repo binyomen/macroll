554546322943e4128d6619ed6b033614ba51e298
Ben Weedon <ben@weedon.email>
2021-03-18 23:01:41 -0700

    Support initiative tracker

--- static/builtins/dnd.js
+++ static/builtins/dnd.js
@@ -51,6 +51,30 @@ macroll.registerMacro('chk', async (name, roll, advantage = 'norm', charname = '
     return await macroll.sendCommand({name: 'simple', fields});
 });
 
+macroll.registerMacro('init', async (mod, advantage = 'norm', charname = '') => {
+    const fields = {
+        rname: 'initiative',
+        mod: mod >= 0 ? `+${mod}` : `${mod}`,
+        normal: 1,
+    };
+
+    let rollString;
+    if (advantage === 'adv') {
+        rollString = `[[2d20kh1 + ${mod} &{tracker}]] (advantage)`;
+    } else if (advantage === 'dadv') {
+        rollString = `[[2d20kl1 + ${mod} &{tracker}]] (disadvantage)`;
+    } else {
+        rollString = `[[1d20 + ${mod} &{tracker}]]`;
+    }
+    fields.r1 = rollString;
+
+    if (charname.length > 0) {
+        fields.charname = charname;
+    }
+
+    return await macroll.sendCommand({name: 'simple', fields});
+});
+
 function getModifier(roll) {
     const inner = roll.inner;
     if (macroll.isOperator(inner) &&
